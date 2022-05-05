fa542653307968853fbdab12c20b02ed1143bb8e
Anant1202 <anantsingh1202@gmail.com>
2022-05-05 19:37:37 +0530

    Welcome to Line Comparison Computation Program

---
+++ Line Comparison Computation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+
+}
+

efb0a7669fd2fe0c6c7fa1ba19aaaeed74a0f889
Anant1202 <anantsingh1202@gmail.com>
2022-05-06 15:27:55 +0530

    Added:Line Comparison

--- Line Comparison Computation/LineComparison.cs
+++ Line Comparison Computation/LineComparison.cs
@@ -24,8 +24,8 @@ namespace LineComparisonComputation
             double Power1 = Math.Pow(x2 - x1, 2);
             double Power2 = Math.Pow(y2 - y1, 2);
             double TotalPow = Power1 + Power2;
-            double Length = Math.Sqrt(TotalPow);
-            Console.WriteLine("Length of the line 1 is " + Length);
+            double Line1 = Math.Sqrt(TotalPow);
+            Console.WriteLine("Length of the line 1 is " + Line1);
 
             Console.WriteLine("---------------------");
             Console.WriteLine("Enter the value of x and y for point 3");
@@ -39,23 +39,38 @@ namespace LineComparisonComputation
             int x4 = Convert.ToInt32(Console.ReadLine());
             Console.Write("y4:");
             int y4 = Convert.ToInt32(Console.ReadLine());
+            double Power3 = Math.Pow(x4 - x3, 2);
+            double Power4 = Math.Pow(y4 - y3, 2);
+            double TotalPow2 = Power3 + Power4;
+            double Line2 = Math.Sqrt(TotalPow2);
+            Console.WriteLine("Length of the line 2 is " + Line2);
 
 
-            if (x1 == x3 && y1 == y3)
+            //if (x1 == x3 && y1 == y3)
+            //{
+            //    if (x2 == x4 && y2 == y4)
+            //    {
+            //        Console.WriteLine("Both lines are equal");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Lines are not equal");
+            //}
+
+
+            if (Line1 > Line2)
             {
-                if (x2 == x4 && y2 == y4)
-                {
-                    Console.WriteLine("Both lines are equal");
-                }
+                Console.WriteLine("Line1 is greater than Line2");
+            }
+            else if (Line1 < Line2)
+            {
+                Console.WriteLine("Line2 is greater than Line1");
             }
             else
             {
-                Console.WriteLine("Lines are not equal");
+                Console.WriteLine("Both Lines are equal");
             }
-
-
-
-            
 
         }
     }
